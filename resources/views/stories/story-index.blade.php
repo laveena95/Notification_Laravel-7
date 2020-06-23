4da3a256527f1f5e72a327e2e6c2e44003b5e9f3
laveena95 <laveenalavee256@gmail.com>
2020-06-23 18:06:37 +0530

    App to check notification

--- resources/views/stories/story-index.blade.php
+++ resources/views/stories/story-index.blade.php
@@ -3,45 +3,45 @@
 @section('pageTitle', '- Stories on my project')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-sm-12">
-            <div class="card">
-                <div class="card-header">
-                    Stories
-                    <span class="float-right"><a href="{{ route('story.add') }}">Add story</a></span>
-                </div>
-                <div class="card-body">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th>Title</th>
-                                <th>Type</th>
-                                <th>Created At</th>
-                            </tr>
-                        </thead>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-header">
+                        Stories
+                        <span class="float-right"><a href="{{ route('story.add') }}">Add story</a></span>
+                    </div>
+                    <div class="card-body">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Title</th>
+                                    <th>Type</th>
+                                    <th>Created At</th>
+                                </tr>
+                            </thead>
 
-                        <tbody>
-                            @foreach($stories as $story)
-                            <tr>
-                                <td>
-                                    <a href="{{route('story.view', $story->id)}}" class="story-title">
-                                        <strong>#{{$story->id}} {{$story->title}}</strong>
-                                    </a>
-                                    <br>
-                                    Created by <a href="#">{{$story->creator->name}}</a>
-                                    <span> Due date {{$story->due_date}}</span>
-                                </td>
-                                <td>{{$story->story_type}}</td>
-                                <td>{{$story->created_at}}</td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                    {{$stories->render()}}
+                            <tbody>
+                                @foreach($stories as $story)
+                                <tr>
+                                    <td>
+                                        <a href="{{route('story.view', $story->id)}}" class="story-title">
+                                            <strong>#{{$story->id}} {{$story->title}}</strong>
+                                        </a>
+                                        <br>
+                                        Created by <a href="#">{{$story->creator->name}}</a>
+                                        <span> Due date {{$story->due_date}}</span>
+                                    </td>
+                                    <td>{{$story->story_type}}</td>
+                                    <td>{{$story->created_at}}</td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        {{$stories->render()}}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
